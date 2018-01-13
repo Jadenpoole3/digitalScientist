24967fe64295b25e72d9eb5e75c3d4d29e020496
Jadenpoole3 <jadenkinpool@yahoo.com>
2018-01-12 20:43:45 -0500

    finished and tested

---
+++ src/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    firebaseConfig: {
+      apiKey: "AIzaSyAx7cVUxhMDrjOX6vjzZVb_6sgxJ1EwW6Y",
+     
+      authDomain: "digital-dash-191922.firebaseapp.com",
+      databaseURL: "https://digital-dash-191922.firebaseio.com",
+      projectId: "digital-dash-191922",
+      storageBucket: "digital-dash-191922.appspot.com",
+      messagingSenderId: "201335673596",
+      
+    },
+    googleCloudVisionAPIKey: "AIzaSyAx7cVUxhMDrjOX6vjzZVb_6sgxJ1EwW6Y",
+  };
